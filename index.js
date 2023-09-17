85cb56d967a28b0b6c4bfca579d9de0c096e5722
Sivaji gadidala <93958018+sivajisj@users.noreply.github.com>
2023-09-17 21:21:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonserver = require('json-server');
 const server = jsonserver.create();
-const router = jsonserver.router("db.json")
+const router = jsonserver.router("data.json")
 const middlew = jsonserver.defaults()
 const port = 3500;
 server.use(middlew)
